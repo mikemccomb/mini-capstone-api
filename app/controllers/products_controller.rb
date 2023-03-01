458b978305f79c9f3d5b223f37c5dafa6f99fbcc
mikemccomb <mikemccomb@gmail.com>
2023-03-01 12:57:16 -0600

    Create individual product routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     @products = Product.all
     render template: "products/index"
   end
+
+  def shirts
+    @product = Product.find_by(name: "T-shirt")
+    render template: "products/show"
+  end
+
+  def pants
+    @product = Product.find_by(name: "Blue Jeans")
+    render template: "products/show"
+  end
+
+  def hats
+    @product = Product.find_by(name: "Hat")
+    render template: "products/show"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "all_products"
+
+  get "/shirts", controller: "products", action: "shirts"
+
+  get "/pants", controller: "products", action: "pants"
+
+  get "/hats", controller: "products", action: "hats"
 end
