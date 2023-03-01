4d33350cfe7dcdeb1045b8e854840e5b42bc9aa7
mikemccomb <mikemccomb@gmail.com>
2023-03-01 12:31:12 -0600

    Create, Migrate, Seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "T-shirt", price: 15, image_url: "", description: "50% Cotton / 50% Human Hair")
+product.save
+product = Product.new(name: "Blue Jeans", price: 25, image_url: "", description: "Denim")
+product.save
+product = Product.new(name: "Hat", price: 10, image_url: "", description: "For your head")
+product.save
