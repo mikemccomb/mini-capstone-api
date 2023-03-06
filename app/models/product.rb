2e6ebe1e82689a4bbe798fbbac8c168972fb5a34
mikemccomb <mikemccomb@gmail.com>
2023-03-06 14:27:50 -0600

    Add model methods and model tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  # You don't need product.method because you are already in the Product class
+  def is_discounted?
+    price <= 10 ? true : false
+  end
+
+  def tax
+    tax = price * 0.09
+  end
+
+  def total
+    total = price + tax
+  end
 end
