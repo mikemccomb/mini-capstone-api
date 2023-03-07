d89d468441118616196544319a84ea2e4213c8cb
mikemccomb <mikemccomb@gmail.com>
2023-03-07 13:46:06 -0600

    Change table columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-product = Product.new(name: "T-shirt", price: 15, image_url: "", description: "50% Cotton / 50% Human Hair")
-product.save
-product = Product.new(name: "Blue Jeans", price: 25, image_url: "", description: "Denim")
-product.save
-product = Product.new(name: "Hat", price: 10, image_url: "", description: "For your head")
-product.save
+Product.create!([
+  { name: "T-shirt", price: "15.0", image_url: "https://cdn.pixabay.com/photo/2014/04/03/10/55/t-shirt-311732_960_720.png", description: "50% Cotton / 50% Human Hair", inventory: 1 },
+  { name: "Blue Jeans", price: "25.0", image_url: "https://cdn.pixabay.com/photo/2014/04/03/00/30/pants-308497_960_720.png", description: "Denim", inventory: 1 },
+  { name: "Hat", price: "10.0", image_url: "https://cdn.pixabay.com/photo/2016/03/31/20/07/bells-1295520_960_720.png", description: "For your head", inventory: 1 },
+  { name: "Socks", price: "8.0", image_url: "https://cdn.pixabay.com/photo/2016/12/14/11/34/socks-1906060_1280.jpg", description: "Do not wear with sandals", inventory: 1 },
+])
