15d115de1a6d56895bbe79aa646c5d3597ba529d
mikemccomb <mikemccomb@gmail.com>
2023-03-08 12:18:58 -0600

    Add Supplier model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-Product.create!([
-  { name: "T-shirt", price: "15.0", image_url: "https://cdn.pixabay.com/photo/2014/04/03/10/55/t-shirt-311732_960_720.png", description: "50% Cotton / 50% Human Hair", inventory: 1 },
-  { name: "Blue Jeans", price: "25.0", image_url: "https://cdn.pixabay.com/photo/2014/04/03/00/30/pants-308497_960_720.png", description: "Denim", inventory: 1 },
-  { name: "Hat", price: "10.0", image_url: "https://cdn.pixabay.com/photo/2016/03/31/20/07/bells-1295520_960_720.png", description: "For your head", inventory: 1 },
-  { name: "Socks", price: "8.0", image_url: "https://cdn.pixabay.com/photo/2016/12/14/11/34/socks-1906060_1280.jpg", description: "Do not wear with sandals", inventory: 1 },
-])
+Supplier.create(name: "Acme, Inc.", email: "roadrunner@acme.com", phone_number: "555-456-7895")
+Supplier.create(name: "Veridian Dynamics", email: "ted@veridian.net", phone_number: "555-698-1345")
+Supplier.create(name: "Oopsie Daisy", email: "oops@oopsie.com", phone_number: "555-356-4519")
