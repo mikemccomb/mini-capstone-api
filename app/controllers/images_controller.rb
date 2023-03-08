6439ba9db3361fa9466f7a3d22f7b431af8f7cfb
mikemccomb <mikemccomb@gmail.com>
2023-03-08 13:53:34 -0600

    Add image seed

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,2 +1,42 @@
 class ImagesController < ApplicationController
+  def index
+    @images = Image.all
+    render :index
+  end
+
+  def show
+    @image = Image.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    @image = Image.new(url: params[:url])
+    @image.save
+
+    if @image.valid?
+      render :show
+    else
+      @image.errors.full_messages
+      render status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @image = Image.find_by(id: params[:id])
+    @image.update(
+      url: params[:url] || @image.url,
+    )
+    if @image.valid?
+      render :show
+    else
+      @image.errors.full_messages
+      render status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    image = Image.find_by(id: params[:id])
+    image.delete
+    render json: { message: "Bye girl bye" }
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
-Supplier.create(name: "Acme, Inc.", email: "roadrunner@acme.com", phone_number: "555-456-7895")
-Supplier.create(name: "Veridian Dynamics", email: "ted@veridian.net", phone_number: "555-698-1345")
-Supplier.create(name: "Oopsie Daisy", email: "oops@oopsie.com", phone_number: "555-356-4519")
+# Supplier.create!([
+#   {name: "Acme, Inc.", email: "roadrunner@acme.com", phone_number: "555-456-7895"},
+#   {name: "Veridian Dynamics", email: "ted@veridian.net", phone_number: "555-698-1345"},
+#   {name: "Oopsie Daisy", email: "oops@oopsie.com", phone_number: "555-356-4519"}
+# ])
+# Product.create!([
+#   {name: "T-shirt", price: "15.0", description: "50% Cotton / 50% Human Hair", inventory: 1, supplier_id: 1},
+#   {name: "Hat", price: "10.0", description: "For your head", inventory: 1, supplier_id: 1},
+#   {name: "Socks", price: "8.0", description: "Do not wear with sandals", inventory: 1, supplier_id: 3},
+#   {name: "Blue Jeans", price: "25.0", description: "Denim", inventory: 1, supplier_id: 2}
+# ])
+Image.create(url: "https://images.unsplash.com/photo-1633966887768-64f9a867bdba")
+Image.create(url: "https://images.unsplash.com/photo-1521369909029-2afed882baee")
+Image.create(url: "https://images.unsplash.com/photo-1582966772680-860e372bb558")
+Image.create(url: "https://images.unsplash.com/photo-1454720503269-3a35c21bebc6")
