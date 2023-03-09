f58fc715cbf8a204b4fb0ccaf0c427759b0aea5b
mikemccomb <mikemccomb@gmail.com>
2023-03-09 13:09:38 -0600

    Adds user create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,15 @@
 Rails.application.routes.draw do
-  #get "/all_products", controller: "products", action: "all_products"
   get "/products" => "products#index"
-
-  # get "/one_product/:id", controller: "products", action: "one_product"
   get "/products/:id" => "products#show"
-
   post "/products" => "products#create"
-
   patch "/products/:id" => "products#update"
-
   delete "/products/:id" => "products#destroy"
 
   get "/suppliers" => "suppliers#index"
-
-  # get "/one_product/:id", controller: "suppliers", action: "one_product"
   get "/suppliers/:id" => "suppliers#show"
-
   post "/suppliers" => "suppliers#create"
-
   patch "/suppliers/:id" => "suppliers#update"
-
   delete "/suppliers/:id" => "suppliers#destroy"
+
+  post "/users" => "users#create"
 end
