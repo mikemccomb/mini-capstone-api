46e1b22099ca3464def9a05092fabc7f750f758e
mikemccomb <mikemccomb@gmail.com>
2023-03-10 14:33:32 -0600

    Attempts to match test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,20 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all
+    render :index
+  end
+
   def create
     @order = Order.new(
       user_id: params[:user_id],
       product_id: params[:product_id],
       quantity: params[:quantity],
-      # subtotal: params[:subtotal],
-      # tax: params[:tax],
-      # total: params[:total],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
     )
     @order.save
-    render :show
+    render json: @order.as_json
   end
 
   def show
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,6 +1,11 @@
-json.user_id user.id
-json.product_id product.id
+json.id order.id
+json.user_id order.user_id
+json.user order.user.name
+json.product_id order.product_id
+json.product order.product
 json.quantity order.quantity
-json.subtotal product.subtotal
-json.tax product.tax
-json.total product.total
+json.subtotal order.subtotal
+json.tax order.tax
+json.total order.total
+json.created_at order.created_at
+json.updated_at order.updated_at
