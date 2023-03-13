f51d4b7c06b77116088ab9663f11f9353b946f79
mikemccomb <mikemccomb@gmail.com>
2023-03-13 13:44:16 -0500

    Add authenticate admin

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -17,7 +17,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   test "create" do
     assert_difference "Order.count", 1 do
       post "/orders.json",
-        params: { product_id: Product.first.id, quantity: @order.quantity, subtotal: @product.price, tax: @product.tax, total: @product.total },
+        params: { product_id: Product.first.id, quantity: 10 },
         headers: { "Authorization" => "Bearer #{@jwt}" }
       assert_response 200
     end
