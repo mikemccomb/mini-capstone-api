7d36fa5e7001e8ad497d6441b61ab2b07e9c2a76
mikemccomb <mikemccomb@gmail.com>
2023-03-16 15:04:36 -0500

    WIP with exercise

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,12 +1,27 @@
 class CartedProductsController < ApplicationController
   def create
     @carted_product = CartedProduct.create(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       product_id: params[:product_id],
-      order_id: params[:order_id],
+      order_id: nil,
       quantity: params[:quantity],
-      status: params[:status],
+      status: "carted",
     )
     render :show
+  end
+
+  def index
+    if current_user
+      @carted_products = CartedProduct.all
+      render :index
+    else
+      render json: [], status: :unauthorized
+    end
+  end
+
+  def destroy
+    @carted_product = CartedProduct.find_by(id: params[:id])
+    @carted_product[:status] = "removed"
+    render json: { message: "Removed from cart" }
   end
 end
